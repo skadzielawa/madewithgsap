da449d0c43112db456bfc8687663a450e00c0752
Szymon Kądzielawa <skadzielawa@gmail.com>
2025-05-13 17:59:36 +0200

    Fix namespace

--- src/000-inertia-plugin/render.php
+++ src/000-inertia-plugin/render.php
@@ -2,7 +2,7 @@
 /**
  * Block render callback.
  *
- * @package MadeWithGSAP
+ * @package Madewithgsap
  * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
  */
 
